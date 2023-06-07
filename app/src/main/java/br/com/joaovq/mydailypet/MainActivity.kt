5c6cb56c246287a882aa838917be2959b461bfbd
joaovq <vitorsnta90@gmail.com>
2023-06-07 14:45:37 -0300

    Initial commit

---
+++ app/src/main/java/br/com/joaovq/mydailypet/MainActivity.kt
@@ -0,0 +1,16 @@
+package br.com.joaovq.mydailypet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.joaovq.mydailypet.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
