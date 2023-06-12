dbd68ef101b6428fa61768bd908d7d87a6a552d9
joaovq <vitorsnta90@gmail.com>
2023-06-12 17:16:26 -0300

    test(android-addpet): create test's for ui

---
+++ app/src/androidTest/java/br/com/joaovq/mydailypet/addpet/presentation/AddPetFragmentTest.kt
@@ -0,0 +1,23 @@
+package br.com.joaovq.mydailypet.addpet.presentation
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import org.junit.After
+import org.junit.Before
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+internal class AddPetFragmentTest {
+
+    /*@get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)*/
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
