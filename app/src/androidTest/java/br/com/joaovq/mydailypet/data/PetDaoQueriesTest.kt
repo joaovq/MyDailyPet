658bca0cdbe67f7748c8d04d4b34fa47454fed62
joaovq <vitorsnta90@gmail.com>
2023-06-29 22:48:33 -0300

    test (pet): create unit and android test's pet

--- app/src/androidTest/java/br/com/joaovq/mydailypet/data/PetDaoQueriesTest.kt
+++ app/src/androidTest/java/br/com/joaovq/mydailypet/data/PetDaoQueriesTest.kt
@@ -10,7 +10,7 @@ import br.com.joaovq.mydailypet.core.data.local.database.MyDailyPetDatabase
 import br.com.joaovq.mydailypet.pet.data.dao.PetDao
 import br.com.joaovq.mydailypet.pet.data.dto.PetDto
 import br.com.joaovq.mydailypet.pet.domain.mappers.toDto
-import br.com.joaovq.mydailypet.pet.domain.model.Pet
+import br.com.joaovq.mydailypet.core.domain.model.Pet
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.flow.firstOrNull
---
+++ app/src/test/java/br/com/joaovq/mydailypet/pet/presentation/viewmodel/PetScreenViewModelTest.kt
@@ -0,0 +1,70 @@
+package br.com.joaovq.mydailypet.pet.presentation.viewmodel
+
+import br.com.joaovq.mydailypet.core.data.local.localdatasource.LocalDataSource
+import br.com.joaovq.mydailypet.pet.data.dto.PetDto
+import br.com.joaovq.mydailypet.pet.presentation.viewintent.PetIntent
+import br.com.joaovq.mydailypet.pet.presentation.viewstate.PetState
+import br.com.joaovq.mydailypet.testutil.TestUtil
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerifyAll
+import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class PetScreenViewModelTest {
+    @MockK
+    private lateinit var localDataSource: LocalDataSource<PetDto>
+    private lateinit var petScreenViewModel: PetScreenViewModel
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        petScreenViewModel = PetScreenViewModel(
+            localDataSource,
+            UnconfinedTestDispatcher(),
+        )
+    }
+
+    @Test
+    fun `GIVEN id WHEN dispatch intent GetPet THEN success`() = runTest {
+        coEvery { localDataSource.getById(1) } returns flow {
+            emit(TestUtil.petDto)
+        }
+        assertEquals(PetState.Initial, petScreenViewModel.state.value)
+        petScreenViewModel.dispatchIntent(PetIntent.GetPetDetails(1))
+        assertEquals(PetState.Success(TestUtil.pet), petScreenViewModel.state.value)
+        coVerifyAll { localDataSource.getById(1) }
+    }
+
+    @Test
+    fun `GIVEN 0 WHEN dispatch intent GetPet throws exception THEN error with message`() = runTest {
+        coEvery { localDataSource.getById(0) } throws Exception("Id not exists in db")
+        assertEquals(PetState.Initial, petScreenViewModel.state.value)
+        petScreenViewModel.dispatchIntent(PetIntent.GetPetDetails(0))
+        assertEquals(PetState.Error("Id not exists in db"), petScreenViewModel.state.value)
+        coVerifyAll { localDataSource.getById(0) }
+    }
+
+    @Test
+    fun `GIVEN 0 WHEN dispatch intent GetPet throws exception THEN error without message`() = runTest {
+        coEvery { localDataSource.getById(0) } throws Exception()
+        assertEquals(PetState.Initial, petScreenViewModel.state.value)
+        petScreenViewModel.dispatchIntent(PetIntent.GetPetDetails(0))
+        assertEquals(PetState.Error("Error"), petScreenViewModel.state.value)
+        coVerifyAll { localDataSource.getById(0) }
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+}
