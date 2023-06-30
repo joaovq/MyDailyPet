7537b444f6436bb23b8add43f4ea64447c84db65
joaovq <vitorsnta90@gmail.com>
2023-06-29 22:49:53 -0300

    test (add pet): update test, add new test

--- app/src/androidTest/java/br/com/joaovq/mydailypet/addpet/presentation/AddPetFragmentTest.kt
+++ app/src/androidTest/java/br/com/joaovq/mydailypet/addpet/presentation/AddPetFragmentTest.kt
@@ -1,8 +1,28 @@
 package br.com.joaovq.mydailypet.addpet.presentation
 
+import android.widget.DatePicker
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.EditorAction
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.swipeUp
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.PickerActions
+import androidx.test.espresso.contrib.ViewPagerActions
+import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withClassName
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import org.junit.After
+import br.com.joaovq.mydailypet.R
+import br.com.joaovq.mydailypet.addpet.presentation.view.AddPetFragment
+import br.com.joaovq.mydailypet.launchFragmentInHiltContainer
+import com.google.android.material.datepicker.MaterialDatePicker
+import org.hamcrest.Matchers
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,19 +31,29 @@ import org.junit.runner.RunWith
 @MediumTest
 internal class AddPetFragmentTest {
 
-    /*@get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)*/
-
     @Before
-    fun setUp() {
+    fun setup() {
+        launchFragmentInHiltContainer<AddPetFragment>()
     }
 
     @Test
-    fun clickDialogSetDate() {
-        /*TODO Use idling and contrib espresso*/
+    fun typeTextInName() {
+        onView(withId(R.id.sv_add_pet))
+            .perform(swipeUp())
+        onView(withId(R.id.et_name_add_pet))
+            .check(matches(isDisplayed()))
+            .perform(ViewActions.typeText("Melquis"))
+            .check(matches(withText("Melquis")))
     }
 
-    @After
-    fun tearDown() {
+    @Test
+    fun typeTextInBreed() {
+        onView(withId(R.id.sv_add_pet))
+            .perform(swipeUp())
+        onView(withId(R.id.atctv_bredd_add_pet))
+            .check(matches(isDisplayed()))
+            .perform(ViewActions.typeText("Schnauzer"))
+        onView(withId(R.id.atctv_bredd_add_pet))
+            .check(matches(withText(R.string.text_schnauzer)))
     }
 }
