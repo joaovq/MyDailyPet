7b7f8b323aaba3457bc12b21d402967447bda5f5
joaovq <vitorsnta90@gmail.com>
2023-07-19 23:25:08 -0300

    refactor (coreTest): update dispatcher rule

--- app/src/test/java/br/com/joaovq/mydailypet/testrule/MainDispatcherRule.kt
+++ app/src/test/java/br/com/joaovq/mydailypet/testrule/MainDispatcherRule.kt
@@ -11,7 +11,7 @@ import org.junit.runner.Description
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class MainDispatcherRule constructor(
-    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
 ) : TestWatcher() {
     override fun starting(description: Description) {
         Dispatchers.setMain(testDispatcher)
