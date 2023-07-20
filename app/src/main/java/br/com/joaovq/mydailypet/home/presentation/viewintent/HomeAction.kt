ecc6ac0210cbe29c2c6251a7139e448c30ca2ec5
joaovq <vitorsnta90@gmail.com>
2023-07-19 23:01:49 -0300

    refactor (home): add navigation for reminder list and swipe for pet details

--- app/src/main/java/br/com/joaovq/mydailypet/home/presentation/viewintent/HomeAction.kt
+++ app/src/main/java/br/com/joaovq/mydailypet/home/presentation/viewintent/HomeAction.kt
@@ -1,5 +1,9 @@
 package br.com.joaovq.mydailypet.home.presentation.viewintent
 
+import br.com.joaovq.mydailypet.pet.domain.model.Pet
+
 sealed interface HomeAction {
     object GetPets : HomeAction
+    object GetReminders : HomeAction
+    data class DeletePet(val pet: Pet) : HomeAction
 }
