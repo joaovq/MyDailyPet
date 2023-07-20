f2e07843daf1aadb41f895f50596fc140e6bf626
joaovq <vitorsnta90@gmail.com>
2023-07-19 22:56:06 -0300

    feature (reminder): create reminder entity and implementation in flow data

---
+++ app/src/main/java/br/com/joaovq/mydailypet/reminder/domain/usecases/EditReminderUseCase.kt
@@ -0,0 +1,23 @@
+package br.com.joaovq.mydailypet.reminder.domain.usecases
+
+import br.com.joaovq.mydailypet.di.DefaultDispatcher
+import br.com.joaovq.mydailypet.reminder.data.repository.ReminderRepository
+import br.com.joaovq.mydailypet.reminder.domain.model.Reminder
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+interface EditReminderUseCase {
+    suspend operator fun invoke(reminder: Reminder)
+}
+
+class EditReminder @Inject constructor(
+    private val reminderRepository: ReminderRepository,
+    @DefaultDispatcher private val dispatcher: CoroutineDispatcher,
+) : EditReminderUseCase {
+    override suspend fun invoke(reminder: Reminder) {
+        withContext(dispatcher) {
+            reminderRepository.updateReminder(reminder)
+        }
+    }
+}
