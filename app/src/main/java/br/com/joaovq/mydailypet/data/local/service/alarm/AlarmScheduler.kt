0c23058c62ce19610892b565b77be3a216a05226
joaovq <vitorsnta90@gmail.com>
2023-07-19 22:42:22 -0300

    feature (data): create alarm scheduler for manage alarm manager schedules

--- app/src/main/java/br/com/joaovq/mydailypet/data/local/service/alarm/AlarmScheduler.kt
+++ app/src/main/java/br/com/joaovq/mydailypet/data/local/service/alarm/AlarmScheduler.kt
@@ -1,8 +1,16 @@
 package br.com.joaovq.mydailypet.data.local.service.alarm
 
+import android.app.AlarmManager
 import br.com.joaovq.mydailypet.data.local.service.alarm.model.AlarmItem
 
 interface AlarmScheduler {
-    fun schedule(alarmItem: AlarmItem)
+    fun scheduleExactAlarmAllowWhileIdle(alarmItem: AlarmItem)
+
+    fun scheduleRepeatAlarm(
+        type: Int = AlarmManager.RTC_WAKEUP,
+        alarmItem: AlarmItem,
+        interval: Long,
+    )
+
     fun cancel(alarmItem: AlarmItem)
 }
--- app/src/main/java/br/com/joaovq/mydailypet/data/local/service/alarm/model/AlarmItem.kt
+++ app/src/main/java/br/com/joaovq/mydailypet/data/local/service/alarm/model/AlarmItem.kt
@@ -1,6 +1,10 @@
 package br.com.joaovq.mydailypet.data.local.service.alarm.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 open class AlarmItem(
     open val time: Long,
     open val message: String,
-)
+) : Parcelable
