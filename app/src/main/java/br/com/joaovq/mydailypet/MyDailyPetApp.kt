24562210a009b8713d72f94c8f9f7c0197bab953
joaovq <joaovqdev@gmail.com>
2024-06-26 12:05:38 -0300

    hot fix: fix crash bug app name

--- app/src/main/java/br/com/joaovq/mydailypet/MyDailyPetApp.kt
+++ app/src/main/java/br/com/joaovq/mydailypet/MyDailyPetApp.kt
@@ -3,6 +3,8 @@ package br.com.joaovq.mydailypet
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import java.util.logging.Level
@@ -15,9 +17,8 @@ class MyDailyPetApp : Application(), Configuration.Provider {
     lateinit var workerFactory: HiltWorkerFactory
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        Firebase.crashlytics.setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
     }
 
 
